ca96c2171e6bcf136c0bb0c6116cf74d948f6782
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-06-19 21:29:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace data_structures_and_algorithms_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
