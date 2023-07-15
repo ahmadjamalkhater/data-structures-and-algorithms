42ad71b288b7c5d1808d3799214fb80245ed11ef
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-07-15 16:48:21 +0300

    ch11

---
+++ challenge11/pseudo queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pseudo_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
