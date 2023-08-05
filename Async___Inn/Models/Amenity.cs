ab2dd881991f1a72c8dd3f51168f94a2859377c5
ahmad jamal <ahmadjamalkhater@gmail.com>
2023-08-05 18:14:19 +0300

    edit file

--- Async___Inn/Models/Amenity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Async___Inn.Models
-{
-    public class Amenity
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public List<RoomAmenities>? RoomAmenities { get; set; }
-    }
-}
